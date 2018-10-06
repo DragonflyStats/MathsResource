adb897ca5c73097b93880e94e70cb19fd59360a5
DragonflyStats <kobriendublin@gmail.com>
2018-10-06 12:06:01 +0100

    reload

---
+++ SULISHomeworkCode/A-floorandceiling-Type1.r
@@ -0,0 +1,17 @@
+myX= sample(0:100,1)/10
+myY= sample(0:100,1)/10
+
+answer=floor(myX)+ ceiling(myY)
+
+cat(" XX This exercise is on the Floor and Ceiling Functions:","\n","\n",
+"XX Suppose you are given the following pieces of information","\n","\n",
+
+" - X is : ", myX , "\n",
+" - Y is : ", myY , "\n","\n",
+
+"XX Add the Floor function of X to the Ceiling Function of Y ", "\n",
+"XX (See the equation below)", "\n",
+"XX Write your answer here:	{  ",	answer,"}","\n","\n","\n")						
+				
+ 
+
